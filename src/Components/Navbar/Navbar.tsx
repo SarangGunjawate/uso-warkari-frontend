4f9f4b69f8b8d014d9ec9873fccf40793b5c4eef
sarang <saranggunjawate9@gmail.com>
2026-04-26 19:14:42 +0530

    fixed issye

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -73,7 +73,7 @@ export default function Navbar() {
                     </NavLink>
 
                     <NavLink
-                        to="/granthved"
+                        to="/pustak-prakashan"
                         onClick={() => setIsOpen(false)}
                         className={({ isActive }) =>
                             `block px-4 py-2 rounded-lg transition-all duration-300 font-medium ${isActive
